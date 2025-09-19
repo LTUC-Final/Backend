f78769aed07796d79d5210a0ffcf9254f61b8872
AdanAljboor <Adan.Aljboorr@gmail.com>
2025-09-20 02:24:49 +0300

    login/register/forget-password + getallwishlist

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ require("dotenv").config();
 const pg = require("pg");
 const cors = require("cors");
 const axios = require("axios");
-
 const express = require("express");
+
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-console.log("Adan Test")
+const registerRoute = require("./routes/register/register");
+const forgotRoute = require("./routes/forgetpassword/forgot");
+const verifyOtpRoute = require("./routes/forgetpassword/verify_otp");
+const resetPasswordRoute = require("./routes/forgetpassword/reset_password");
+const loginRoute = require("./routes/login/login");
+const logoutRoute = require("./routes/login/logout");
+const wishlistRoute = require("./routes/wishlist/getAll");
 
+app.use("/api", registerRoute);
+app.use("/api", forgotRoute);
+app.use("/api", verifyOtpRoute);
+app.use("/api", resetPasswordRoute);
+app.use("/api", loginRoute);
+app.use("/api", logoutRoute);
+app.use("/api", wishlistRoute);
 
 app.use((req, res) => {
   res.status(404).send("Page not fond <a href='/'>back to home </a>");
@@ -24,15 +38,10 @@ pool
       .query("SELECT current_database(), current_user")
       .then((res) => {
         client.release();
-
         const dbName = res.rows[0].current_database;
         const dbUser = res.rows[0].current_user;
         console.log(" Connected to DB:", res.rows[0]);
-
-        console.log(
-          `Connected to PostgreSQL as user '${dbUser}' on database '${dbName}'`
-        );
-
+        console.log(`Connected to PostgreSQL as user '${dbUser}' on database '${dbName}'`);
         console.log(`App listening on port http://localhost:${port}`);
       });
   })
@@ -44,3 +53,9 @@ pool
   .catch((err) => {
     console.error("Could not connect to database:", err);
   });
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Rejection:", reason);
+});
+
+module.exports = { app, pool };
