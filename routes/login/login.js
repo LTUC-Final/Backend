cb1bf85aea2802a7429e5bb482e86ebaca4c7a76
AdanAljboor <Adan.Aljboorr@gmail.com>
2025-09-20 12:42:24 +0300

    The Last update

--- routes/login/login.js
+++ routes/login/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+//Login page 
 const pg = require("pg");
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
