4d98f11199ee5606099ad19d1e93894247373756
hussam-200 <hussamibrahim434@gmail.com>
2025-09-22 21:53:27 +0300

    new push of backend folder

--- routes/UserDashboard/ShowCardInUserDashboard.js
+++ routes/UserDashboard/ShowCardInUserDashboard.js
@@ -8,9 +8,11 @@ route.get("/ShowCardInUserDashboard", async (req, res) => {
 
   try {
 
-    const card = await pool.query(`SELECT s.product_id , p.provider_id ,u.firstname , u.lastname, s.category_id , s.name , s.location , s.price , s.image 
+    const card = await pool.query(`SELECT s.product_id , p.provider_id ,u.firstname , u.lastname, s.category_id , s.name , s.location , s.price , s.image , g.name AS category_name,
+    g.description AS category_description
       FROM products s 
       JOIN providers p ON p.provider_id = s.provider_id 
+      JOIN categories g ON g.category_id = s.category_id
       JOIN users u ON u.user_id = p.user_id`)
 
     if (card.rows.length === 0) {
