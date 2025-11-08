bd2d0544ff1ff4cd0c6b450d4cf4a803c918f4a8
hussam-200 <hussamibrahim434@gmail.com>
2025-11-08 15:08:56 +0300

    asd

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "*", // change to your frontend domain
+  origin: "https://your-frontend.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
