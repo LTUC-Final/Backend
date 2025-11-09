dfb8b962b6c1d1cba698f6b68b211457764ac13c
hussam-200 <hussamibrahim434@gmail.com>
2025-11-09 21:24:33 +0300

    asdas

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,27 +1,27 @@
-// // firebaseConfig.js
-// const admin = require("firebase-admin");
-// const path = require("path");
+// firebaseConfig.js
+const admin = require("firebase-admin");
+const path = require("path");
 
-// const serviceAccount = require(path.join(
-//   __dirname,
-//   "job-tracker-b9e24-firebase-adminsdk-fbsvc-33c4e692a1.json"
-// ));
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   // storageBucket: "job-tracker-b9e24.appspot.com",
-//   storageBucket: "job-tracker-b9e24.firebasestorage.app",
-// });
-
-// const bucket = admin.storage().bucket();
-
-// module.exports = { bucket };
-const admin = require('firebase-admin');
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require(path.join(
+  __dirname,
+  "job-tracker-b9e24-firebase-adminsdk-fbsvc-33c4e692a1.json"
+));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  // storageBucket: "job-tracker-b9e24.appspot.com",
+  storageBucket: "job-tracker-b9e24.firebasestorage.app",
 });
 
-module.exports = admin;
+const bucket = admin.storage().bucket();
+
+module.exports = { bucket };
+// const admin = require('firebase-admin');
+
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = admin;
